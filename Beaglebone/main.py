4cd224153118aea15b512598306d9d22367186d8
Ryan Kane <rakane@syr.edu>
2020-02-09 03:24:40 +0000

    Beaglebone: Formatting

--- Beaglebone/main.py
+++ Beaglebone/main.py
@@ -7,29 +7,34 @@ while True:
     for i in range(0, 361):
 
         # Measurement command test
+        print("------------------------------")
         print("Requesting measurement...")
         distance = serialHandler.getDistance(i)
         if(distance < 0):
             print("Error! No response from Arduino!")
         else:
-            print(distance)
-        time.sleep(1)
+            print("Response Distance: " + str(distance))
+        print("------------------------------")
+        time.sleep(5)
         
         # Movement command test
+        print("------------------------------")
         print("Sending Movement...")
         res = serialHandler.sendMovement(1.0)
         if(res):
             print("Success!")
         else:
             print("Failed!")
-        time.sleep(1)
+        print("------------------------------")
+        time.sleep(5)
         
         # Rotation command test
+        print("------------------------------")
         print("Sending rotation...")
         res = serialHandler.sendRotation(3.0)
         if(res):
             print("Success!")
         else:
             print("Failed!")
-
+        print("------------------------------")
         time.sleep(5)
