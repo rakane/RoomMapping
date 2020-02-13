0c2a6c2f50b01dbc9ea9ea7cc804d24ac06ad554
rakane <rakane@syr.edu>
2020-02-12 23:01:06 -0500

    Arduino: Correctly rotates servo to desired angle steps of 10 degrees

--- Arduino/room-mapping/room-mapping.ino
+++ Arduino/room-mapping/room-mapping.ino
@@ -2,11 +2,9 @@
 #include <Servo.h>
 
 #define DIR_CW 0
-#define DIR_ACW 1
+#define DIR_CCW 1
 
 // Communication
-void onPacketReceived(const uint8_t* buffer, size_t size);
-
 PacketSerial_<COBS> myPacketSerial;
 
 typedef union _packet {
@@ -14,13 +12,15 @@ typedef union _packet {
   uint8_t packet_data[4];
 };
 
+_packet errorPacket;
+
 int currentOperation = -1;
 
 // Servo Control
 Servo myservo;
 
-uint16_t fullyCW = 1000; //ACW meaning anti-clockwise (counter clockwise)
-uint16_t fullyACW = 2000;
+uint16_t fullyCW = 1000;    // Counter-Clockwise
+uint16_t fullyCCW = 2000;   // Clockwise
 
 uint8_t servoControlPin = 9;
 uint8_t servoFeedbackPin = 5;
@@ -28,7 +28,7 @@ uint8_t servoFeedbackPin = 5;
 int feedbackLow = 33;
 int feedbackHigh = 1050;
 float commandPos = 180.f;
-float errorMargin = .6f;
+float errorMargin = 0.6f;
 int microsecondRange = 1000;
 int microsecondMin = 1000;
 int microsecondCenter = 1500;
@@ -39,8 +39,10 @@ int pulseinVal = 0;
 
 //Meta Values
 int totalRange = feedbackHigh - feedbackLow;
-float pulsePerDeg = totalRange/360.f;
+float pulsePerDeg = totalRange / 360.f;
 
+// Functions
+void onPacketReceived(const uint8_t* buffer, size_t size);
 int servoMoveHoming(float _commandPos);
 float feedback2Deg(float _inputPulse);
 int servoMoveCW(float _commandPos);
@@ -49,31 +51,41 @@ int servoMoveACW(float _commandPos);
 
 
 void setup() {
+  // Servo setup
   pinMode(servoFeedbackPin, INPUT);
-  myservo.attach(servoControlPin);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(servoControlPin);  
+
+  // Servo initial position
   pulseinVal = pulseIn(servoFeedbackPin, HIGH);
   currentPos = feedback2Deg(pulseinVal);
+
+  // Communication setup
   Serial.begin(9600);
   Serial2.begin(9600);
   myPacketSerial.setStream(&Serial2);
   myPacketSerial.setPacketHandler(&onPacketReceived);
-  
-// Stop on 89 - 95
-// 88 - X CW
-// 96 - X CCW  
-//  sensorServo.attach(sensorServoPin);
-//  sensorServo.write(96);
+  errorPacket.packet_float = -1;
+
+  // Stop on 89 - 95
+  // 88 - X CW
+  // 96 - X CCW
+  //  sensorServo.attach(sensorServoPin);
+  //  sensorServo.write(96);
 }
 
 void loop() {
+
+  // Check for incoming packets
   myPacketSerial.update();
 
+  // Check for buffer overflow
   if (myPacketSerial.overflow()) {
     Serial.println("Buffer Overflow!");
   }
 }
 
 
+// Handles communication with Beaglbone
 void onPacketReceived(const uint8_t* buffer, size_t size) {
   uint8_t tempBuffer[size];
   memcpy(tempBuffer, buffer, size);
@@ -97,23 +109,37 @@ void onPacketReceived(const uint8_t* buffer, size_t size) {
   if (currentOperation == 0)
   {
     Serial.println("Current Operation: Gather Data");
+
+    // Convert uint8_t array to float
     for (int i = 0; i < 4; i++) {
       incomingPacket.packet_data[i] = tempBuffer[i];
     }
     Serial.print("Angle to measure: ");
     Serial.println(incomingPacket.packet_float);
-    servoMoveHoming((int) incomingPacket.packet_float);
-    myPacketSerial.send(incomingPacket.packet_data, 4);
+
+    // Rotate sensor servo motor
+    int status = servoMoveHoming((int) incomingPacket.packet_float);
+
+    // if status = -1, then an error occured and data wasn't retrieved
+    if (status == -1) {
+      myPacketSerial.send(errorPacket.packet_data, 4);
+    } else {
+      myPacketSerial.send(incomingPacket.packet_data, 4);
+    }
   }
   else if (currentOperation == 1)
   {
     Serial.println("Current Operation: Move");
+
+    // Convert uint8_t array to float
     for (int i = 0; i < 4; i++) {
       incomingPacket.packet_data[i] = tempBuffer[i];
     }
+    
     Serial.print("Distance: ");
     Serial.println(incomingPacket.packet_float);
 
+    // Send response packet
     _packet sendPacket;
     sendPacket.packet_float = 1.0;
     myPacketSerial.send(sendPacket.packet_data, 4);
@@ -121,12 +147,16 @@ void onPacketReceived(const uint8_t* buffer, size_t size) {
   else if (currentOperation == 2)
   {
     Serial.println("Current Operation: Rotate");
+
+    // Convert uint8_t array to float
     for (int i = 0; i < 4; i++) {
       incomingPacket.packet_data[i] = tempBuffer[i];
     }
+    
     Serial.print("Angle to Rotate: ");
     Serial.println(incomingPacket.packet_float);
 
+    // Send response packet
     _packet sendPacket;
     sendPacket.packet_float = 1.0;
     myPacketSerial.send(sendPacket.packet_data, 4);
@@ -134,18 +164,21 @@ void onPacketReceived(const uint8_t* buffer, size_t size) {
   Serial.println("---------------------------------------");
 }
 
-float feedback2Deg(float _inputPulse){
-  float outDeg =  (_inputPulse-feedbackLow)/pulsePerDeg;
+// Calculates Servo motor angle from feedback
+float feedback2Deg(float _inputPulse) {
+  float outDeg =  (_inputPulse - feedbackLow) / pulsePerDeg;
   return outDeg;
 }
 
-int servoMoveHoming(float _commandPos){
+// Handles angle calculations for servo rotations
+int servoMoveHoming(float _commandPos) {
 
-  //Current Position
+  // Get current Servo position
   pulseinVal = pulseIn(servoFeedbackPin, HIGH);
   currentPos = feedback2Deg(pulseinVal);
 
-  while(abs(_commandPos - currentPos) > errorMargin){
+  // Stay in loop until error is acceptable
+  while (abs(_commandPos - currentPos) > errorMargin) {
     //Determine which direction to rotate
 
     //Create copies of our actual values
@@ -153,80 +186,115 @@ int servoMoveHoming(float _commandPos){
     float limitedCurrentPos = currentPos;
 
     //Give them floors and cielings for the sake of repeatibility
-    if(limitedCommandPos < 0.f){
+    if (limitedCommandPos < 0.f) {
       limitedCommandPos = 0.f;
-    } else if(limitedCommandPos > 360.f) {
+    } else if (limitedCommandPos > 360.f) {
       limitedCommandPos = 360.f;
     }
 
-    if(limitedCurrentPos < 0.f){
+    if (limitedCurrentPos < 0.f) {
       limitedCurrentPos = 0.f;
-    } else if(limitedCurrentPos > 360.f) {
-      limitedCurrentPos = 360.f;
+    } else if (limitedCurrentPos > 360.f) {
+      limitedCurrentPos = 0.f;
     }
+    
+    if (limitedCommandPos < limitedCurrentPos) {
+      Serial.println("CCW");
+      int status = servoMove(_commandPos, DIR_CCW);
 
-    //Determine if the shortest path is going to be clockwise or counter clockwise
-    if(limitedCommandPos > limitedCurrentPos){
-      if((limitedCommandPos - limitedCurrentPos) > 180.f){
-        servoMove(_commandPos, DIR_ACW);
-      } else {
-        servoMove(_commandPos, DIR_CW);
+      // Status = -1 means an error occured
+      if (status == -1) {
+        return -1;
       }
     } else {
-      if((limitedCurrentPos - limitedCommandPos) < 180.f){
-        servoMove(_commandPos, DIR_ACW);
-      } else {
-        servoMove(_commandPos, DIR_CW);
+      Serial.println("CW");
+      int status = servoMove(_commandPos, DIR_CW);
+
+      // Status = -1 means an error occured
+      if (status == -1) {
+        return -1;
       }
     }
 
+
+    //Determine if the shortest path is going to be clockwise or counter clockwise
+    //    if(limitedCommandPos > limitedCurrentPos){
+    //      if((limitedCommandPos - limitedCurrentPos) > 180.f){
+    //        servoMove(_commandPos, DIR_ACW);
+    //      } else {
+    //        servoMove(_commandPos, DIR_CW);
+    //      }
+    //    } else {
+    //      if((limitedCurrentPos - limitedCommandPos) < 180.f){
+    //        servoMove(_commandPos, DIR_ACW);
+    //      } else {
+    //        servoMove(_commandPos, DIR_CW);
+    //      }
+    //    }
+
+    // Calculate new positions
     pulseinVal = pulseIn(servoFeedbackPin, HIGH);
     currentPos = feedback2Deg(pulseinVal);
   }
-  
-  
+
+
   return 0;
 }
 
-int servoMove(float _commandPos, int _dir){
-  do{
-
-    //Get the current position of the servo horn
+int servoMove(float _commandPos, int _dir) {
+  // Timeout control variables
+  unsigned long startTime = millis();
+  unsigned long currTime = millis();
+  
+  do {
+    // Calculate current Servo position
     pulseinVal = pulseIn(servoFeedbackPin, HIGH);
     currentPos = feedback2Deg(pulseinVal);
 
-    //Formulate a magnitude based upon how farway the horn is away from its desired position
-    float absMag = (abs(currentPos - _commandPos) / 360.f) * (microsecondRange/2.f);
+    // Formulate a magnitude based upon how farway the Servo is away from its desired position
+    float absMag = (abs(currentPos - _commandPos) / 360.f) * (microsecondRange / 2.f);
 
-    //Let's set the speed based on how big our magnitude is. The resulting effect should be that if there is
-    //a lot of distance the servo should move fast, but smaller distances it will go slower. This code puts
-    //a floor and a cieling on the servo speed.
+    // Let's set the speed based on how big our magnitude is. The resulting effect should be that if there is
+    // a lot of distance the servo should move fast, but smaller distances it will go slower. This code puts
+    // a floor and a ceiling on the servo speed.
     int force = 0;
-    if(DIR_CW == _dir){
+    if (DIR_CW == _dir) {
 
+      // Add floor and ceiling check
       force = microsecondCenter - absMag;
-      if(force < microsecondMin){
+      if (force < microsecondMin) {
         force = microsecondMin;
-      } else if(force > microsecondCenter - minpulse){
+      } else if (force > microsecondCenter - minpulse) {
         force = microsecondCenter - minpulse;
       }
 
     } else {
 
+      // Add floor and ceiling check
       force = microsecondCenter + absMag;
-      if(force > microsecondMax){
+      if (force > microsecondMax) {
         force = microsecondMax;
-      } else if(force < microsecondCenter + minpulse){
+      } else if (force < microsecondCenter + minpulse) {
         force = microsecondCenter + minpulse;
       }
     }
 
-    //We move the servo
+    // We move the servo
     myservo.writeMicroseconds(force);
     
-  }while(abs(currentPos - _commandPos) > errorMargin);
+    currTime = millis();
 
+    // If reducing the error below the acceptable level takes longer
+    // than 2 seconds, break and return -1 indicating an error occured
+    if (currTime - startTime > 2000) {
+      Serial.print("Final Error: ");
+      Serial.println(abs(currentPos - _commandPos));
+      return -1;
+    }
+  } while (abs(currentPos - _commandPos) > errorMargin);
+
+  // Stop Servo at correct position using center
   myservo.writeMicroseconds(microsecondCenter);
-  
+
   return 0;
 }
